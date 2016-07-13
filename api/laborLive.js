e4ed60eaf117d3a5497fcdc44c103e802258e8d9
jinniw43805 <jinniw43805@gmail.com>
2016-07-13 13:54:56 +0800

    FIX : change socket.io sturuct

--- api/laborLive.js
+++ api/laborLive.js
@@ -13,7 +13,7 @@ function  rethinkDbListener(r,connection) {
           // socket.on('connect', function (socket) {
           //     console.log('Server Connected!');
           // });
-          socket.emit('toClient', { hello: item });
+          socket.emit('news', { hello: item });
           // myio.emit('news',{hello:'tony'});
           // console.log(item);
         }
