7d1dc431fd057e63902f56f3d5f802e934b204c7
jinniw43805 <jinniw43805@gmail.com>
2016-07-13 11:05:56 +0800

    MODIFY:try to fix middle

--- api/app.js
+++ api/app.js
@@ -62,9 +62,10 @@ app.use('/hots', hots);
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
-  res.setHeader('Access-Control-Allow-Origin', "*");
-  res.setHeader('Access-Control-Allow-Methods', "*");
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
   next(err);
 });
 
